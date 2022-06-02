7c57539384569058092e9cc395a7053dc0464c93
henzo <84385799+henzopuchetti@users.noreply.github.com>
2022-06-02 12:27:25 -0300

    Add files via upload

---
+++ routes/atividades.js
@@ -0,0 +1,26 @@
+const { request } = require("express")
+
+module.exports = (app)=>{
+    app.post("/atividades",async(req,res)=>{
+        var dados = req.body
+        //console.log(dados)
+        //conectar com o database
+        const database = require("../config/database")()
+        //importar o model atividades
+        const atividades = require("../models/atividades")
+        //gravar as informações do formulario no database
+        var gravar = await new atividades({
+            data:dados.data,
+            tipo:dados.tipo,
+            entrega:dados.entrega,
+            disciplina:dados.disciplina,
+            instrucoes:dados.orientacoes,
+            usuario:dados.id,
+            titulo:dados.titulo
+        }).save()
+        //buscar as atividades do usuario
+        var buscar = await atividades.find({usuario:dados.id})
+        //recarregar a página atividades
+        res.render("atividades.ejs",{nome:dados.nome, id:dados.id,lista:buscar})
+    })
+}
